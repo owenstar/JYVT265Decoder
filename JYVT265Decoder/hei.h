b34937f493040b90b87e55a700bcce24d4f51fee
xiaobai <>
2019-01-28 17:33:24 +0800

    hei

---
+++ JYVT265Decoder/hei.h
@@ -0,0 +1,13 @@
+//
+//  hei.h
+//  hahah
+//
+//  Created by nihuo on 2019/1/24.
+//  Copyright © 2019年 nihuo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface hei : NSObject
+-(void)hahahahha;
+@end
---
+++ JYVT265Decoder/hei.m
@@ -0,0 +1,16 @@
+//
+//  hei.m
+//  hahah
+//
+//  Created by nihuo on 2019/1/24.
+//  Copyright © 2019年 nihuo. All rights reserved.
+//
+
+#import "hei.h"
+
+@implementation hei
+-(void)hahahahha
+{
+    NSLog(@"%s",__func__);
+}
+@end
